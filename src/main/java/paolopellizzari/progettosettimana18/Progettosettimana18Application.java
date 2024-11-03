93c1c93a0861abee586bbf6d0c6e753abf09cb35
oBeta97 <paolopellizzari97@gmail.com>
2024-11-03 13:52:37 +0100

    init progetto

---
+++ src/main/java/paolopellizzari/progettosettimana18/Progettosettimana18Application.java
@@ -0,0 +1,13 @@
+package paolopellizzari.progettosettimana18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettosettimana18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettosettimana18Application.class, args);
+	}
+
+}
---
+++ src/test/java/paolopellizzari/progettosettimana18/Progettosettimana18ApplicationTests.java
@@ -0,0 +1,13 @@
+package paolopellizzari.progettosettimana18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettosettimana18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
